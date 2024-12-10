a33a0c95ec3f6a15c4c631b8969bfe5975bc03ca
neecride <neecride@gmail.com>
2024-12-10 11:29:45 +0100

    Update roundednavbar.js

--- navigation/roundednavbar/js/roundednavbar.js
+++ navigation/roundednavbar/js/roundednavbar.js
@@ -40,13 +40,6 @@ function boxNavBar() {
   $('.header-right').css({
     'margin-top': '0'
   });
-  $('.cold').css({
-    'background-color':'transparent',
-    'background-image': 'url(../roundednavbar/img/bg-menu.jpg)',
-    'background-repeat': 'repeat-x',
-    'background-position': '50%',
-    'transition': '.2s'
-  });
 }
 
 function roundedNav() {
@@ -70,10 +63,6 @@ function roundedNav() {
   });
   $('.header-right').css({
     'margin-top': '10px'
-  });
-  $('.cold').css({
-    'background-color':'transparent',
-    'background-image': 'none'
   });
 }
 
